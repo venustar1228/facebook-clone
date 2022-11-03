1ab887af7ed064b18c0fab7a4a572406eea83a73
venustar1228 <venustar-1228@outlook.com>
2022-11-03 02:41:24 -0400

    left sidebar finished

--- components/LeftSideBar.js
+++ components/LeftSideBar.js
@@ -9,20 +9,49 @@ import Image from 'next/image';
 
 function LeftSideBar() {
   return (
-    <div className='w-[5rem]'>
-        <div className='flex flex-col space-y-6 pl-5 pt-5'>
-            <MdHome className='w-8 h-8'/>
-            <div>
-                <Image src={user} className='rounded-full w-10 h-10'/>
+    <div className='w-[10rem] hidden sm:block'>
+        <div className='flex flex-col pl-5 pt-5'>
+            <div className='flex items-center'>
+                <MdHome className='w-9 h-9'/>
+                <p className='ml-2 font-bold'>Home</p>
             </div>
-            <div className='border-b' />
-            <AiOutlineDesktop className='w-8 h-8'/>
-            <BsPeopleFill className='w-8 h-8'/>
-            <MdGroups className='w-8 h-8'/>
-            <BsCart3 className='w-7 h-7'/>
-            <BsCalendar2Fill className='w-8 h-8'/>
-            <AiFillClockCircle className='w-8 h-8'/>
-            <RiArrowDownSLine className='w-8 h-8'/>
+            <div className='flex items-center mt-4'>
+                <div className='w-10 h-10'>
+                    <Image src={user} className='rounded-full'/>
+                </div>
+                <p className='ml-2 font-bold'>Log In</p>
+            </div>
+            <div className='border-b my-4' />
+            <div className='space-y-5'>
+                <div className='flex items-center'>
+                    <AiOutlineDesktop className='w-8 h-8'/>
+                    <p className='ml-2 font-bold'>Watch</p>
+                </div>
+                <div className='flex items-center'>
+                    <BsPeopleFill className='w-8 h-8'/>
+                    <p className='ml-2 font-bold'>Friends</p>
+                </div>
+                <div className='flex items-center'>
+                    <MdGroups className='w-8 h-8'/>
+                    <p className='ml-2 font-bold'>Groups</p>
+                </div>
+                <div className='flex items-center'>
+                    <BsCart3 className='w-8 h-8'/>
+                    <p className='ml-2 font-bold'>Marketplace</p>
+                </div>
+                <div className='flex items-center'>
+                    <BsCalendar2Fill className='w-8 h-8'/>
+                    <p className='ml-2 font-bold'>Events</p>
+                </div>
+                <div className='flex items-center'>
+                    <AiFillClockCircle className='w-8 h-8'/>
+                    <p className='ml-2 font-bold'>Memories</p>
+                </div>
+                <div className='flex items-center'>
+                    <RiArrowDownSLine className='w-8 h-8'/>
+                    <p className='ml-2'>See more</p>
+                </div>
+            </div>
         </div>
     </div>
   )
